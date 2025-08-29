5e8666958f098c1bf5e062f9f86b2fad792e8de5
Anderson Galvis <stiven.g2031@gmail.com>
2025-08-29 15:35:52 -0500

    Se crea la estructura basica para el proyecto, añadimos de una vez todas las dependencias

---
+++ README.md
@@ -0,0 +1,4 @@
+agregamos el archivo para ignorar la subida de las cosas importantes .gitignore
+
+creamos archivo requeriments.txt para las dependencias que necesitaremos 
+
---
+++ app/config.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Carga el archivo .env automáticamente
+
+class Settings:
+    # JWT
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+
+    # DB
+    DB_HOST: str = os.getenv("DB_HOST")
+    DB_PORT: str = os.getenv("DB_PORT")
+    DB_USER: str = os.getenv("DB_USER")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
+    DB_NAME: str = os.getenv("DB_NAME")
+
+    # Otros
+    ENV: str = os.getenv("ENV", "development")
+    DEBUG: bool = os.getenv("DEBUG", "true").lower() == "true"
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return (
+            f"mysql+pymysql://{self.DB_USER}:{self.DB_PASSWORD}"
+            f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        )
+
+settings = Settings()
